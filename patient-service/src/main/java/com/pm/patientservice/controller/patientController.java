2f73d8841cb6f223efc6048b44cf8d34c6809e2e
Anant Saxena <anantsaxena5454@gmail.com>
2026-04-03 00:08:42 +0530

    added the create and update route

--- patient-service/src/main/java/com/pm/patientservice/controller/patientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/patientController.java
@@ -1,17 +1,20 @@
 package com.pm.patientservice.controller;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.dto.validators.CreatePatientValidationGroups;
 import com.pm.patientservice.service.patientService;
+import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
@@ -24,13 +27,19 @@ public class patientController {
 
     @GetMapping("/patients")
     public ResponseEntity<List<PatientResponseDTO>> getPatients(){
-        try{
-            List<PatientResponseDTO> patients = patientService.getPatients();
-            return new ResponseEntity<>(patients, HttpStatus.OK);
-        }
-        catch(Exception e){
-            return  new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        List<PatientResponseDTO> patients = patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
     }
 
+    @PostMapping("/registerPatient")
+    public ResponseEntity<PatientResponseDTO> registerPatient(@Validated({Default.class, CreatePatientValidationGroups.class}) @RequestBody PatientRequestDTO patient) {
+        PatientResponseDTO Patient = patientService.registerPatient(patient);
+        return ResponseEntity.ok().body(Patient);
+    }
+
+    @PutMapping("/updatePatient/{Id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID Id,@Validated({Default.class}) @RequestBody PatientRequestDTO patient){
+        PatientResponseDTO Patient = patientService.updatePatient(Id,patient);
+        return ResponseEntity.ok().body(Patient);
+    }
 }
--- patient-service/src/main/java/com/pm/patientservice/repository/patientRepository.java
+++ patient-service/src/main/java/com/pm/patientservice/repository/patientRepository.java
@@ -1,14 +1,15 @@
 package com.pm.patientservice.repository;
 
 import com.pm.patientservice.model.Patient;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
 public interface patientRepository extends JpaRepository<Patient, UUID> {
-    
+
+    boolean existsByEmail(String email);
+
+    boolean existsByEmailAndIdNot(String Email,UUID Id);
 }
