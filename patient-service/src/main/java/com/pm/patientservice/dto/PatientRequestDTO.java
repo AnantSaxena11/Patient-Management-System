4cae6c5010305848120dd13b52a4f83b1e83971c
Anant Saxena <anantsaxena5454@gmail.com>
2026-04-04 01:41:33 +0530

    Improved the API Documentation part

--- patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -1,6 +1,7 @@
 package com.pm.patientservice.dto;
 
 import com.pm.patientservice.dto.validators.CreatePatientValidationGroups;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -11,23 +12,28 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(name = "PatientRequestDTO" , description="PatientRequestDTO Class")
 public class PatientRequestDTO {
     @NotBlank(message = "Name is required")
     @Size(max = 100 , message = "Name cannot exceed 100 characters")
+    @Schema(name = "Name", description = "Patient Name" ,  example = "John Doe")
     private String name;
 
     @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
+    @Schema(name = "email" , description = "Patient Email" , example = "example@mail.com")
     private String email;
 
     @NotBlank(message = "Address is required")
+    @Schema(name = "address" , description = "Patient Address" , example = "Street X City Y")
     private String address;
 
     @NotBlank(message = "Date of Birth is required")
+    @Schema(name = "Date of Birth" , description = "Patients DOB" , example = "2003-10-11")
     private String dateOfBirth;
 
-
     @NotBlank(groups = CreatePatientValidationGroups.class, message = "Registration date is required")
+    @Schema(name = "Date of Registration" , description = "Patients registration date" , example = "2026-10-11")
     private String registrationDate;
 
 }
